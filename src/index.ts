94d55278ba222acd811a11ff368ab0ab3fb9eb02
Diana Vidal <dvidaal@gmail.com>
2023-03-06 20:18:14 +0100

    Add virtuals for mongoose in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import "./loadEnvironment.js";
+import mongoose from "mongoose";
+import connectDataBase from "./database/connectDataBase.js";
 
-const a = 0;
+const mongoUrl = process.env.MONGODB_CONNECTION_URL!;
+
+mongoose.set("toJSON", {
+  virtuals: true,
+  transform(doc, ret) {
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+await connectDataBase(mongoUrl);
