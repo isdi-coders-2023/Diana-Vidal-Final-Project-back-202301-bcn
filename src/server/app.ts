f8594dc65ab867b49d1addc569ffa0c73c5d4218
Diana Vidal <dvidaal@gmail.com>
2023-03-07 09:18:29 +0100

    Configure cors for origins in app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 export const app = express();
 app.disable("x-powered-by");
+
+const allowedOrigins = [
+  process.env.CORS_ALLOWED_ORIGIN_LOCAL!,
+  process.env.CORS_ALLOWED_ORIGIN_PRODUCTION!,
+];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
